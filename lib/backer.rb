c0ddbe17480be918a1c8937300aea8cf9b0f8a19
karen ercoli <karenercoli@gmail.com>
2019-01-13 17:41:42 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -4,4 +4,8 @@ class Backer
     @name = name
     @backed_projects = []
   end
+  def name=(name)
+       @name = name
+       #I believe this is where I change @name instance variable
+   end
 end
